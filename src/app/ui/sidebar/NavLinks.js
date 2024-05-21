4782ec67a4911432cfa11fb7110610cda025865a
AnthonyNassif <anthony.a.nassif@gmail.com>
2024-05-21 10:38:53 +0300

    adding the sauce

--- src/app/ui/sidebar/NavLinks.js
+++ src/app/ui/sidebar/NavLinks.js
@@ -36,7 +36,7 @@ const links = [
             <Link 
               key={link.name}
               href = {link.href}
-              className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-orange-300 p-3 text-sm font-medium hover:bg-sky-100 hover:text-amber-600 md:flex-none md:justify-start md:p-2 md:px-3"
+              className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-orange-500 p-3 text-sm font-medium hover:bg-sky-100 hover:text-amber-600 md:flex-none md:justify-start md:p-2 md:px-3"
             >
               <IconComponent className="w-6"/>
 
--- src/app/ui/sidebar/Sidebar.js
+++ src/app/ui/sidebar/Sidebar.js
@@ -7,14 +7,14 @@ const Sidebar = () => {
     <div>
 
         <Link href="/store"
-            className="bg-amber-900 flex h-20 items-end justify-start rounded-md p-4"
+            className="bg-amber-900 mb-4 flex h-20 items-end justify-start rounded-md p-4"
         >
             <div className="w-32 md:w-40" >
               <Logo/>      
             </div>
         </Link>
 
-        <div>
+        <div className="flex flex-wrap gap-2 justify-center md:grow flex-row md:justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2" >
 
             <NavLinks/>
 
